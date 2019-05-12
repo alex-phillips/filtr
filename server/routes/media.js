4c8e225ea370ae0107552f91097dca505947f4a4
alex-phillips <ahp118@gmail.com>
2019-05-12 15:26:49 -0400

    added route to get all media as well as to updating individual media assets

--- server/routes/media.js
+++ server/routes/media.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models/index')
 
+router.get('/', (req, res, next) => {
+  db.Media.findAll()
+    .then(media => res.json(media))
+})
+
 router.get('/:id', (req, res, next) => {
   db.Media.findOne({
     where: {
@@ -30,6 +35,24 @@ router.get('/:id/thumb', (req, res, next) => {
   })
     .then(media => media.getThumbnail())
     .then(thumbnail => res.sendFile(thumbnail))
+})
+
+router.put('/:id', (req, res, next) => {
+  db.Media.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(media => {
+      for (let prop of [
+        'name',
+        'description'
+      ]) {
+        media[prop] = req.body[prop] || media[prop]
+      }
+
+      return media.save().then(() => res.json(media))
+    })
 })
 
 module.exports = router
