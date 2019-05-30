08df5addeb4b8ab75d99e183ec5262e6b87764f3
alex-phillips <ahp118@gmail.com>
2019-05-30 07:47:37 -0400

    properly returning a function to allow passing in id into getter

--- src/store/tags/getters.js
+++ src/store/tags/getters.js
@@ -2,13 +2,15 @@ export function getTags (state) {
   return state.tags
 }
 
-export function getTagByID (state, id) {
-  let retval = state.tags.filter(t => t.id === id)
-  if (retval.length === 1) {
-    return retval[0]
-  }
+export function getTagByID (state) {
+  return id => {
+    let retval = state.tags.filter(t => t.id === id)
+    if (retval.length === 1) {
+      return retval[0]
+    }
 
-  return null
+    return null
+  }
 }
 
 export function getTagsByIDs (state, getters) {
