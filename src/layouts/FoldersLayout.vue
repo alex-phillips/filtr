6180e6933136c6d6a5864fa535c8551be76fcff7
alex-phillips <ahp118@gmail.com>
2019-05-31 11:49:47 -0400

    drawer is now in overlay mode to prevent having to resize the justified layout grid view

--- src/layouts/FoldersLayout.vue
+++ src/layouts/FoldersLayout.vue
@@ -7,7 +7,7 @@
       :breakpoint="700"
       show-if-above
       elevated
-
+      overlay
     >
       <q-scroll-area class="fit">
         <q-input ref="filter" filled v-model="filter" label="Filter" @keyup.esc="filter = ''">
